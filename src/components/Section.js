1c1c772b4e622cee187af0ade094587f39be004d
Daniel <tushinskij.danya@gmail.com>
2023-06-01 15:06:04 +0300

    END

--- src/components/Section.js
+++ src/components/Section.js
@@ -3,6 +3,44 @@ import { useState } from "react";
 function Section (props) {
     const [firstCard, setCard] = useState(props.card);
 
+    function changeFavourite () {
+        setCard((copyCard) => {
+            let card = {...copyCard};
+            card.favourite = !card.favourite;
+            return card;
+        })
+    }
+
+    function minus () {
+        setCard((copyCard) => {
+            let card = {...copyCard};
+            card.basket = false;
+            if (card.count >= 2) {
+                card.count -= 1;
+            }
+            return card;
+        })
+    }
+
+    function plus () {
+        setCard((copyCard) => {
+            let card = {...copyCard};
+            card.basket = false;
+            if (card.count >= 1) {
+                card.count += 1;
+            }
+            return card;
+        })
+    }
+
+    function putBasket () {
+        setCard((copyCard) => {
+            let card = {...copyCard};
+            card.basket = true;
+            return card;
+        })
+    }
+
     return (
         <div className="card">
             <div className="pictureComplete">
@@ -10,10 +48,26 @@ function Section (props) {
                 <div className="sticking">
                     <span className="sticking-name">Новинка</span>
                 </div>
-                {/* <div className="like">
+                <div className="like">
                     <img onClick={changeFavourite} src={firstCard.favourite ? firstCard.likeActive : firstCard.likeDefault} alt="like" />
-                </div> */}
+                </div>
             </div> 
+            <div className="card-content">
+                <p className="title">{firstCard.title}</p>
+                <p className="prise">{firstCard.prise * firstCard.count} ₽</p>
+                <div className="counter">
+                    <div><button onClick={minus}>-</button></div>
+                    <div className="counter-constructor">
+                        <p className={firstCard.basket ? "quantity none" : "quantity"}>{firstCard.count}</p>
+                        <a onClick={putBasket} className={firstCard.basket ? "basket-button none" : "basket-button"}>Добавить в корзину!</a>
+                        <div className={firstCard.basket ? "basket" : "basket none"}>
+                            <p className="basket-count">{firstCard.count}шт</p>
+                            <p className="basket-title">В корзине</p>
+                        </div>
+                    </div>
+                    <div><button onClick={plus}>+</button></div>
+                </div>
+            </div>
         </div>
     )
 }
